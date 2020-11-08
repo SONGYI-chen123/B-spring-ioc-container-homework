f45bd4b05efab4e2df64c7cdc4beb8513bf9b18d
SONGYI-chen123 <song1577660501@163.com>
2020-11-08 22:35:02 +0800

    [syc]:finished p2s1

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,8 +1,13 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.context.annotation.Scope;
+
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
 
 @Service
+@Scope(SCOPE_PROTOTYPE)
 public class GreetingService {
 
     String sayHi() {
